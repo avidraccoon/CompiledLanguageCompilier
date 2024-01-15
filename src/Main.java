6759474264d52cff34a40cc464e0d832a2ebe54d
natha <nathansmith@mcps.org>
2024-01-14 21:16:45 -0500

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("src/code.txt");
+        Parser.Parse(fileInputStream);
+
+    }
+}
